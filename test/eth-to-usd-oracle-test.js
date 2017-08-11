b2f4029af2ba723ff36802f8b132b0feba78980c
Hassan Abdel-Rahman <hassan.abdelrahman@mhelabs.com>
2017-08-11 11:54:43 -0400

    reorganize tests

--- test/eth-to-usd-oracle-test.js
+++ test/eth-to-usd-oracle-test.js
@@ -33,145 +33,145 @@ contract("EthToUsdOracle", function(accounts) {
       assert.ok(lastUpdate.toNumber(), "The last update time was set");
       assert.equal(requestEvent.event, "NewOraclizeQuery", "the oraclize request event was issued");
     });
-  });
 
-  it("should automatically update the ETH price", async function() {
-    let owner = accounts[0];
-    let oracleBalance = web3.toWei(0.1, "ether");
-    let oracle = await EthToUsdOracle.new(true, {
-      from: owner,
-      value: oracleBalance
-    });
-
-    let firstEvent = await waitForContractEvent(oracle, "ETHPriceUpdated");
-    let firstUpdateTime = await oracle.lastUpdate();
-
-    await oracle.setUpdateFrequency(2);
-
-    await new Bluebird.Promise(res => setTimeout(() => res(), 500));
-
-    let secondEvent = await waitForContractEvent(oracle, "ETHPriceUpdated");
-    let secondUpdateTime = await oracle.lastUpdate();
-
-    assert.notEqual(firstEvent.blockNumber, secondEvent.blockNumber, "The price update block heights are different");
-    assert.notEqual(firstUpdateTime.toNumber(), secondUpdateTime.toNumber(), "The price update times are different");
-
-    await oracle.setUpdateFrequency(3600); // be kind to the local oraclize, as recursive updates can't be cancelled
-  });
-
-  it("should not update the price faster than updateFrequencySeconds", async function() {
-    let owner = accounts[0];
-    let oracleBalance = web3.toWei(0.1, "ether");
-    let oracle = await EthToUsdOracle.new(true, {
-      from: owner,
-      value: oracleBalance
-    });
-
-    await waitForContractEvent(oracle, "ETHPriceUpdated");
-
-    await new Bluebird.Promise(res => setTimeout(() => res(),  500));
-
-    let updateTxn = await oracle.update(0);
-
-    let nothing = await waitForContractEvent(oracle, "ETHPriceUpdated", 30)
-      .then(() => assert.ok(false, "Expect timeout exception waiting for event that is never fired"))
-      .catch(() => { /* expect timeout exception  */});
-
-    assert.isUndefined(nothing, "no price update event was received");
-    assert.equal(_.filter(updateTxn.logs, { event: "ETHPriceUpdated" }).length, 0, 'No price event was logged in update txn');
-  });
-
-
-  it("should fire an OraclizeQueryNotEnoughFunds event if there is not enough balance to fund the call to get a price update", async function() {
-    let oracle = await EthToUsdOracle.new(true);
-
-    await waitForContractEvent(oracle, "ETHPriceUpdated"); // Oraclize uses gas from the contract creation for the first request
-    let lastUpdate = await oracle.lastUpdate();
-    let ethPrice = await oracle.ETHUSD();
-
-    await oracle.setUpdateFrequency(2);
-    await new Bluebird.Promise(res => setTimeout(() => res(), 5 * 1000));
-
-    let updateTxn = await oracle.update(0);
-
-    let unchangedLastUpdate = await oracle.lastUpdate();
-    let unchangedEthPrice = await oracle.ETHUSD();
-
-    assert.equal(updateTxn.logs.length, 1, 'There is an event');
-    assert.equal(updateTxn.logs[0].event, "OraclizeQueryNotEnoughFunds", "the not enough funds event was fired");
-    assert.equal(lastUpdate.toNumber(), unchangedLastUpdate.toNumber(), "The price update date did not change");
-    assert.equal(ethPrice.toNumber(), unchangedEthPrice.toNumber(), "The price did not change");
-
-    await oracle.setUpdateFrequency(3600); // be kind to the local oraclize, as recursive updates can't be cancelled
-  });
-
-  it("Callback function cannot be invoked by non-oraclize address", async function() {
-    let owner = accounts[0];
-    let oracleBalance = web3.toWei(0.1, "ether");
-    let oracle = await EthToUsdOracle.new(true, {
-      from: owner,
-      value: oracleBalance
-    });
-
-    await waitForContractEvent(oracle, "ETHPriceUpdated");
-
-    let exceptionThrown;
-    try {
-      await oracle.__callback("0x3a6c8157a8dc8bda28e5ed2023653a6b080b17696add3481dee168b367a8a15b","306.18988","QmfNEh4BBredUiyvg9MKv34QxAeLm5Spir72ifv5geysT2");
-    } catch (e) {
-      exceptionThrown = true;
-    }
-    assert.ok(exceptionThrown, "exception is thrown invoking the oraclize callback");
-  });
-
-  it("Non-owner should not be able to change the update frequency", async function() {
-    let owner = accounts[0];
-    let nonOwner = accounts[1];
-    let oracleBalance = web3.toWei(0.1, "ether");
-    let oracle = await EthToUsdOracle.new(true, {
-      from: owner,
-      value: oracleBalance
-    });
-
-    await waitForContractEvent(oracle, "ETHPriceUpdated");
-
-    let exceptionThrown;
-    try {
-      await oracle.setUpdateFrequency(2, {
-        from: nonOwner
+    it("should automatically update the ETH price", async function() {
+      let owner = accounts[0];
+      let oracleBalance = web3.toWei(0.1, "ether");
+      let oracle = await EthToUsdOracle.new(true, {
+        from: owner,
+        value: oracleBalance
       });
-    } catch (e) {
-      exceptionThrown = true;
-    }
-    assert.ok(exceptionThrown, "exception is thrown when non-owner tries to change update frequency");
 
-    let updateFrequencySeconds = await oracle.updateFrequencySeconds();
+      let firstEvent = await waitForContractEvent(oracle, "ETHPriceUpdated");
+      let firstUpdateTime = await oracle.lastUpdate();
 
-    assert.equal(updateFrequencySeconds.toNumber(), 3600, 'the update frequency is set to the default value');
-  });
+      await oracle.setUpdateFrequency(2);
 
-  it("Cannot set update frequency to 0 sec", async function() {
-    let owner = accounts[0];
-    let oracleBalance = web3.toWei(0.1, "ether");
-    let oracle = await EthToUsdOracle.new(true, {
-      from: owner,
-      value: oracleBalance
+      await new Bluebird.Promise(res => setTimeout(() => res(), 500));
+
+      let secondEvent = await waitForContractEvent(oracle, "ETHPriceUpdated");
+      let secondUpdateTime = await oracle.lastUpdate();
+
+      assert.notEqual(firstEvent.blockNumber, secondEvent.blockNumber, "The price update block heights are different");
+      assert.notEqual(firstUpdateTime.toNumber(), secondUpdateTime.toNumber(), "The price update times are different");
+
+      await oracle.setUpdateFrequency(3600); // be kind to the local oraclize, as recursive updates can't be cancelled
     });
 
-    await waitForContractEvent(oracle, "ETHPriceUpdated");
+    it("should not update the price faster than updateFrequencySeconds", async function() {
+      let owner = accounts[0];
+      let oracleBalance = web3.toWei(0.1, "ether");
+      let oracle = await EthToUsdOracle.new(true, {
+        from: owner,
+        value: oracleBalance
+      });
 
-    let exceptionThrown;
-    try {
-      await oracle.setUpdateFrequency(0);
-    } catch (e) {
-      exceptionThrown = true;
-    }
-    assert.ok(exceptionThrown, "exception is thrown when update frequency is set to 0");
+      await waitForContractEvent(oracle, "ETHPriceUpdated");
 
-    let updateFrequencySeconds = await oracle.updateFrequencySeconds();
+      await new Bluebird.Promise(res => setTimeout(() => res(),  500));
 
-    assert.equal(updateFrequencySeconds.toNumber(), 3600, 'the update frequency is set to the default value');
+      let updateTxn = await oracle.update(0);
+
+      let nothing = await waitForContractEvent(oracle, "ETHPriceUpdated", 30)
+        .then(() => assert.ok(false, "Expect timeout exception waiting for event that is never fired"))
+        .catch(() => { /* expect timeout exception  */});
+
+      assert.isUndefined(nothing, "no price update event was received");
+      assert.equal(_.filter(updateTxn.logs, { event: "ETHPriceUpdated" }).length, 0, 'No price event was logged in update txn');
+    });
+
+
+    it("should fire an OraclizeQueryNotEnoughFunds event if there is not enough balance to fund the call to get a price update", async function() {
+      let oracle = await EthToUsdOracle.new(true);
+
+      await waitForContractEvent(oracle, "ETHPriceUpdated"); // Oraclize uses gas from the contract creation for the first request
+      let lastUpdate = await oracle.lastUpdate();
+      let ethPrice = await oracle.ETHUSD();
+
+      await oracle.setUpdateFrequency(2);
+      await new Bluebird.Promise(res => setTimeout(() => res(), 5 * 1000));
+
+      let updateTxn = await oracle.update(0);
+
+      let unchangedLastUpdate = await oracle.lastUpdate();
+      let unchangedEthPrice = await oracle.ETHUSD();
+
+      assert.equal(updateTxn.logs.length, 1, 'There is an event');
+      assert.equal(updateTxn.logs[0].event, "OraclizeQueryNotEnoughFunds", "the not enough funds event was fired");
+      assert.equal(lastUpdate.toNumber(), unchangedLastUpdate.toNumber(), "The price update date did not change");
+      assert.equal(ethPrice.toNumber(), unchangedEthPrice.toNumber(), "The price did not change");
+
+      await oracle.setUpdateFrequency(3600); // be kind to the local oraclize, as recursive updates can't be cancelled
+    });
+
+    it("should not allow callback function to be invoked by non-oraclize address", async function() {
+      let owner = accounts[0];
+      let oracleBalance = web3.toWei(0.1, "ether");
+      let oracle = await EthToUsdOracle.new(true, {
+        from: owner,
+        value: oracleBalance
+      });
+
+      await waitForContractEvent(oracle, "ETHPriceUpdated");
+
+      let exceptionThrown;
+      try {
+        await oracle.__callback("0x3a6c8157a8dc8bda28e5ed2023653a6b080b17696add3481dee168b367a8a15b","306.18988","QmfNEh4BBredUiyvg9MKv34QxAeLm5Spir72ifv5geysT2");
+      } catch (e) {
+        exceptionThrown = true;
+      }
+      assert.ok(exceptionThrown, "exception is thrown invoking the oraclize callback");
+    });
+
+    it("should not allow non-owner be able to change the update frequency", async function() {
+      let owner = accounts[0];
+      let nonOwner = accounts[1];
+      let oracleBalance = web3.toWei(0.1, "ether");
+      let oracle = await EthToUsdOracle.new(true, {
+        from: owner,
+        value: oracleBalance
+      });
+
+      await waitForContractEvent(oracle, "ETHPriceUpdated");
+
+      let exceptionThrown;
+      try {
+        await oracle.setUpdateFrequency(2, {
+          from: nonOwner
+        });
+      } catch (e) {
+        exceptionThrown = true;
+      }
+      assert.ok(exceptionThrown, "exception is thrown when non-owner tries to change update frequency");
+
+      let updateFrequencySeconds = await oracle.updateFrequencySeconds();
+
+      assert.equal(updateFrequencySeconds.toNumber(), 3600, 'the update frequency is set to the default value');
+    });
+
+    it("should not allow update frequency to be set to 0 sec", async function() {
+      let owner = accounts[0];
+      let oracleBalance = web3.toWei(0.1, "ether");
+      let oracle = await EthToUsdOracle.new(true, {
+        from: owner,
+        value: oracleBalance
+      });
+
+      await waitForContractEvent(oracle, "ETHPriceUpdated");
+
+      let exceptionThrown;
+      try {
+        await oracle.setUpdateFrequency(0);
+      } catch (e) {
+        exceptionThrown = true;
+      }
+      assert.ok(exceptionThrown, "exception is thrown when update frequency is set to 0");
+
+      let updateFrequencySeconds = await oracle.updateFrequencySeconds();
+
+      assert.equal(updateFrequencySeconds.toNumber(), 3600, 'the update frequency is set to the default value');
+    });
+
   });
-
 
 });
