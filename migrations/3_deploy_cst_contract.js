654c28f26ed380569fcbb38eafc6a7708e8312e6
Hassan Abdel-Rahman <hassan.abdelrahman@mhelabs.com>
2017-08-11 11:46:04 -0400

    fix typo;

--- migrations/3_deploy_cst_contract.js
+++ migrations/3_deploy_cst_contract.js
@@ -1,5 +1,5 @@
 let SafeMath = artifacts.require("./open-zepplin/math/SafeMath.sol");
-let CardStackToken = artifacts.require("./CardSTackToken.sol");
+let CardStackToken = artifacts.require("./CardStackToken.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(SafeMath);
