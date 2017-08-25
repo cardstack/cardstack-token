614fc8068e2d6c45caeb40c7e8e50861dee4415d
Will Bagby <bagsbee@gmail.com>
2017-08-25 08:55:36 -0400

    External Storage tests

--- test/external-storage-test.js
+++ test/external-storage-test.js
@@ -1,78 +1,284 @@
-// const CardStackToken = artifacts.require("./CardStackToken.sol");
+const ExternalStorage = artifacts.require("./ExternalStorage.sol");
 
-contract('ExternalStorage', function(/*accounts*/) {
+contract('ExternalStorage', function(accounts) {
+  let storage;
+  let owner = accounts[0];
+  let admin = accounts[3];
 
   describe("external storage", function() {
-    xit("allows owner to add an admin", async function() {
+    beforeEach(async function() {
+      storage = await ExternalStorage.new();
     });
 
-    xit("allows owner to remove an admin", async function() {
+    it("allows owner to add an admin", async function() {
+      await storage.addAdmin(admin);
+      let isAdmin = await storage.admins(admin);
+
+      assert.ok(isAdmin, "admin was added");
     });
 
-    xit("non-owner cannot add admins", async function() {
+    it("allows owner to remove an admin", async function() {
+      await storage.addAdmin(admin);
+      let isAdmin = await storage.admins(admin);
+
+      assert.ok(isAdmin, "admin was added");
+
+      await storage.removeAdmin(admin);
+      isAdmin = await storage.admins(admin);
+
+      assert.notOk(isAdmin, "admin was removed");
     });
 
-    xit("allows admin to set uint value", async function () {
+    it("non-owner cannot add admins", async function() {
+      let nonOwner = accounts[7];
+      let exceptionThrown;
+
+      try {
+        await storage.addAdmin(admin, { from: nonOwner });
+      } catch(err) {
+        exceptionThrown = true;
+      }
+
+      isAdmin = await storage.admins(admin);
+
+      assert.ok(exceptionThrown, "Exception was thrown");
+      assert.notOk(isAdmin, "admin was not added");
     });
 
-    xit("does not allow non-admin to set uint value", async function() {
+    it("allows admin to set uint value", async function () {
+      await storage.addAdmin(admin);
+      await storage.setUIntValue("cstBuyPrice", 11, { from: admin });
+
+      let storageBuyPrice = await storage.getUIntValue("cstBuyPrice");
+      assert.equal(storageBuyPrice, 11, "uint value was set by admin");
     });
 
-    xit("gets uint value", async function () {
+    it("does not allow non-admin to set uint value", async function() {
+      let nonAdmin = accounts[17];
+      let exceptionThrown;
+
+      try {
+        await storage.setUIntValue("cstBuyPrice", 11, { from: nonAdmin });
+      } catch(e) {
+        exceptionThrown = true;
+      }
+
+      assert.ok(exceptionThrown, "exception was thrown trying to set uint as non-admin");
+      let storageBuyPrice = await storage.getUIntValue("cstBuyPrice");
+      assert.equal(storageBuyPrice.toNumber(), 0, "uint value was not set");
     });
 
-    xit("allows admin to set bytes32 value", async function () {
+    it("gets uint value", async function () {
+      let someUser = accounts[13];
+
+      await storage.addAdmin(admin);
+      await storage.setUIntValue("cstBuyPrice", 11, { from: admin });
+
+      let storageBuyPrice = await storage.getUIntValue("cstBuyPrice", { from: someUser });
+      assert.equal(storageBuyPrice, 11, "unit value was getted by non-admin");
     });
 
-    xit("does not allow non-admin to set bytes32 value", async function() {
+    it("allows admin to set bytes32 value", async function () {
+      await storage.addAdmin(admin);
+      await storage.setBytes32Value("cstTokenSymbol", web3.toHex("CST"), { from: admin });
+
+      let storageTokenSymbol = await storage.getBytes32Value("cstTokenSymbol");
+      assert.equal(web3.toUtf8(storageTokenSymbol.toString()), "CST", "bytes32 value was set by admin");
     });
 
-    xit("gets ledger balance value", async function () {
+    it("does not allow non-admin to set bytes32 value", async function() {
+      let nonAdmin = accounts[17];
+      let exceptionThrown;
+
+      try {
+        await storage.setBytes32Value("cstTokenSymbol", web3.toHex("CST"), { from: nonAdmin });
+      } catch(e) {
+        exceptionThrown = true;
+      }
+
+      assert.ok(exceptionThrown, "exception was thrown trying to set bytes32 as non-admin");
+      let storageTokenSymbol = await storage.getBytes32Value("cstTokenSymbol");
+      assert.equal(web3.toUtf8(storageTokenSymbol.toString()), "", "bytes32 value was not set");
     });
 
-    xit("allows admin to set ledger balance value", async function () {
+    it("gets bytes32 value", async function () {
+      let someUser = accounts[13];
+
+      await storage.addAdmin(admin);
+      await storage.setBytes32Value("cstTokenSymbol", web3.toHex("CST"), { from: admin });
+
+      let storageTokenSymbol = await storage.getBytes32Value("cstTokenSymbol", { from: someUser });
+      assert.equal(web3.toUtf8(storageTokenSymbol.toString()), "CST", "bytes32 value was getted by non-admin");
     });
 
-    xit("does not allow non-admin to set ledger balance value", async function() {
+    it("allows admin to set ledger balance value", async function () {
+      let someUser = accounts[13];
+
+      await storage.addAdmin(admin);
+      await storage.setBalance(someUser, 200, { from: admin });
+
+      let storageBalance = await storage.getBalanceFor(someUser);
+      assert.equal(storageBalance, 200, "balance value was set by admin");
     });
 
-    xit("gets bytes32 value", async function () {
+    it("does not allow non-admin to set ledger balance value", async function() {
+      let someUser = accounts[13];
+      let exceptionThrown;
+
+      try {
+        await storage.setBalance(someUser, 200, { from: someUser });
+      } catch(e) {
+        exceptionThrown = true;
+      }
+
+      assert.ok(exceptionThrown, "exception was thrown trying to set balance as non-admin");
+      let storageBalance = await storage.getBalanceFor(someUser);
+      assert.equal(storageBalance, 0, "balance value was not set by non-admin");
     });
 
-    xit("allows admin to set address value", async function () {
+    it("gets ledger balance value", async function () {
+      let someUser = accounts[13];
+
+      await storage.addAdmin(admin);
+      await storage.setBalance(someUser, 200, { from: admin });
+
+      let storageBalance = await storage.getBalanceFor(someUser, { from: someUser });
+      assert.equal(storageBalance, 200, "balance value was getted by non-admin");
     });
 
-    xit("does not allow non-admin to set address value", async function() {
+    it("allows admin to set address value", async function () {
+      let rewardPool = accounts[8];
+
+      await storage.addAdmin(admin);
+      await storage.setAddressValue("cstRewardPool", rewardPool, { from: admin });
+
+      let storageRewardPool = await storage.getAddressValue("cstRewardPool");
+      assert.equal(storageRewardPool, rewardPool, "address value was set by admin");
     });
 
-    xit("gets address value", async function () {
+    it("does not allow non-admin to set address value", async function() {
+      let rewardPool = accounts[8];
+      let someUser = accounts[13];
+      let exceptionThrown;
+
+      try {
+        await storage.setAddressValue("cstRewardPool", rewardPool, { from: someUser });
+      } catch(e) {
+        exceptionThrown = true;
+      }
+
+      assert.ok(exceptionThrown, "exception was thrown trying to set address as non-admin");
+      let storageRewardPool = await storage.getBalanceFor(someUser);
+      assert.equal(storageRewardPool.toString(), "0", "address value was not set by non-admin");
     });
 
-    xit("allows admin to set bytes value", async function () {
+    it("gets address value", async function () {
+      let rewardPool = accounts[8];
+      let someUser = accounts[13];
+
+      await storage.addAdmin(admin);
+      await storage.setAddressValue("cstRewardPool", rewardPool, { from: admin });
+
+      let storageRewardPool = await storage.getAddressValue("cstRewardPool", { from: someUser });
+      assert.equal(storageRewardPool, rewardPool, "address value was getted by non-admin");
     });
 
-    xit("does not allow non-admin to set bytes value", async function() {
+    it("allows admin to set bytes value", async function () {
+      await storage.addAdmin(admin);
+      await storage.setBytesValue("somebytes", "lmnop", { from: admin });
+
+      let storageSomebytes = await storage.getBytesValue("somebytes");
+      assert.equal(web3.toUtf8(storageSomebytes.toString()), "lmnop", "bytes value was set by admin");
     });
 
-    xit("gets bytes value", async function () {
+    it("does not allow non-admin to set bytes value", async function() {
+      let someUser = accounts[11];
+      let exceptionThrown;
+
+      try {
+        await storage.setBytesValue("somebytes", "lmnop", { from: someUser });
+      } catch(e) {
+        exceptionThrown = true;
+      }
+
+      assert.ok(exceptionThrown, "exception was thrown trying to set bytes as non-admin");
+      let storageSomebytes = await storage.getBytesValue("somebytes");
+      assert.equal(web3.toUtf8(storageSomebytes.toString()), "", "bytes value was not set by non-admin");
     });
 
-    xit("allows admin to set boolean value", async function () {
+    it("gets bytes value", async function () {
+      let someUser = accounts[11];
+
+      await storage.addAdmin(admin);
+      await storage.setBytesValue("somebytes", "lmnop", { from: admin });
+
+      let storageSomebytes = await storage.getBytesValue("somebytes", { from: someUser });
+      assert.equal(web3.toUtf8(storageSomebytes.toString()), "lmnop", "bytes value was getted by non-admin");
     });
 
-    xit("does not allow non-admin to set boolean value", async function() {
+    it("allows admin to set boolean value", async function () {
+      await storage.addAdmin(admin);
+      await storage.setBooleanValue("somebool", true, { from: admin });
+
+      let storageSomebool = await storage.getBooleanValue("somebool");
+      assert.equal(storageSomebool, true, "boolean value was set by admin");
     });
 
-    xit("gets boolean value", async function () {
+    it("does not allow non-admin to set boolean value", async function() {
+      let someUser = accounts[11];
+      let exceptionThrown;
+
+      try {
+        await storage.setBooleanValue("somebool", true, { from: someUser });
+      } catch(e) {
+        exceptionThrown = true;
+      }
+
+      assert.ok(exceptionThrown, "exception was thrown trying to set boolean as non-admin");
+      let storageSomebool = await storage.getBooleanValue("somebool");
+      assert.notOk(storageSomebool, "boolean value was not set by non-admin");
     });
 
-    xit("allows admin to set int value", async function () {
+    it("gets boolean value", async function () {
+      let someUser = accounts[11];
+
+      await storage.addAdmin(admin);
+      await storage.setBooleanValue("somebool", true, { from: admin });
+
+      let storageSomebool = await storage.getBooleanValue("somebool", { from: someUser });
+      assert.equal(storageSomebool, true, "boolean value was getted by non-admin");
     });
 
-    xit("does not allow non-admin to set int value", async function() {
+    it("allows admin to set int value", async function () {
+      await storage.addAdmin(admin);
+      await storage.setIntValue("someint", 37, { from: admin });
+
+      let storageSomeint = await storage.getIntValue("someint");
+      assert.equal(storageSomeint, 37, "int value was set by admin");
     });
 
-    xit("gets int value", async function () {
+    it("does not allow non-admin to set int value", async function() {
+      let someUser = accounts[11];
+      let exceptionThrown;
+
+      try {
+        await storage.setIntValue("someint", 37, { from: someUser });
+      } catch(e) {
+        exceptionThrown = true;
+      }
+
+      let storageSomeint = await storage.getIntValue("someint");
+      assert.equal(storageSomeint, 0, "int value was not set by non-admin");
+    });
+
+    it("gets int value", async function () {
+      let someUser = accounts[11];
+
+      await storage.addAdmin(admin);
+      await storage.setIntValue("someint", 37, { from: admin });
+
+      let storageSomeint = await storage.getIntValue("someint", { from: someUser });
+      assert.equal(storageSomeint, 37, "int value was getted by non-admin");
     });
   });
 });
