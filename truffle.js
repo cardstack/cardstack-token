cbbfa3cb4556774aea45d110129eaa53181d78ab
Hassan Abdel-Rahman <hassan.abdelrahman@mhelabs.com>
2017-08-31 17:26:39 -0400

    fixed CST initialization in migration

--- truffle.js
+++ truffle.js
@@ -1,6 +1,14 @@
 module.exports = {
   networks: {
+    // this is for auotmated testing and development
     development: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*" // Match any network id
+    },
+
+    // this is for testing migration and mist in testrpc
+    testrpc: {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
