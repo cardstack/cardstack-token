c195a036ea7ec59f55e43727b3da5d9c80d8c30f
Hassan Abdel-Rahman <hassan.abdelrahman@mhelabs.com>
2017-09-06 12:52:28 -0400

    removing magic strings

--- migrations/4_deploy_cst_contract.js
+++ migrations/4_deploy_cst_contract.js
@@ -1,3 +1,4 @@
+const { CST_STORAGE_NAME, CST_LEDGER_NAME } = require("../lib/constants");
 let RegistryContract = artifacts.require("./Registry.sol");
 let CstLibrary = artifacts.require("./CstLibrary.sol");
 let CardStackToken = artifacts.require("./CardStackToken.sol");
@@ -11,6 +12,6 @@ module.exports = async function(deployer) {
     let registry = await RegistryContract.deployed();
     await deployer.deploy(CstLibrary);
     await deployer.link(CstLibrary, CardStackToken);
-    await deployer.deploy(CardStackToken, registry.address, "cstStorage", "cstLedger");
+    await deployer.deploy(CardStackToken, registry.address, CST_STORAGE_NAME, CST_LEDGER_NAME);
   }
 };
