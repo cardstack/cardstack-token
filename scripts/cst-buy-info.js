885c14eb84fab1b8db9b21f954c1a384d1dc9f7a
Hassan Abdel-Rahman <hassan.abdelrahman@mhelabs.com>
2017-09-08 17:12:21 -0400

    added script to show CST purcahse info and fixed data bugs

---
+++ scripts/cst-buy-info.js
@@ -0,0 +1,54 @@
+const { CST_NAME } = require("../lib/constants");
+const commandLineArgs = require('command-line-args');
+const getUsage = require('command-line-usage');
+let RegistryContract = artifacts.require("./Registry.sol");
+let CardStackToken = artifacts.require("./CardStackToken.sol");
+
+const optionsDefs = [
+  { name: "help", alias: "h", type: Boolean },
+  { name: "network", type: String },
+  { name: "registry", alias: "r", type: String }
+];
+
+const usage = [
+  {
+    header: "cst-buy-info",
+    content: "This script display purchase information that instructs how to buy CST."
+  },{
+    header: "Options",
+    optionList: [{
+      name: "help",
+      alias: "h",
+      description: "Print this usage guide."
+    },{
+      name: "network",
+      description: "The blockchain that you wish to use. Valid options are `testrpc`, `rinkeby`, `mainnet`."
+    },{
+      name: "registry",
+      alias: "r",
+      description: "The address of the registry."
+    }]
+  }
+];
+
+module.exports = async function(callback) {
+  const options = commandLineArgs(optionsDefs);
+
+  if (!options.network || options.help || !options.registry) {
+    console.log(getUsage(usage));
+    callback();
+    return;
+  }
+
+  let registryAddress = options.registry;
+
+  let registry = registryAddress ? await RegistryContract.at(registryAddress) : await RegistryContract.deployed();
+
+  console.log(`Using registry at ${registry.address}`);
+  let cstAddress = await registry.contractForHash(web3.sha3(CST_NAME));
+
+  let cst = await CardStackToken.at(cstAddress);
+
+
+  callback();
+};
