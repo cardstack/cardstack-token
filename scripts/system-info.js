d4656e287ecd8c575880f52d7c4546e1bf0ef6d8
Hassan Abdel-Rahman <hassan.abdelrahman@gmail.com>
2017-10-10 13:57:13 -0400

    fix issues in system info script

--- scripts/system-info.js
+++ scripts/system-info.js
@@ -5,7 +5,7 @@ let RegistryContract = artifacts.require("./Registry.sol");
 let ExternalStorage = artifacts.require("./ExternalStorage.sol");
 let CstLedger = artifacts.require("./CstLedger.sol");
 
-const { NULL_ADDRESS, CST_NAME } = require("../lib/constants");
+const { NULL_ADDRESS, CST_NAME, CST_LEDGER_NAME, CST_STORAGE_NAME } = require("../lib/constants");
 
 const optionsDefs = [
   { name: "help", alias: "h", type: Boolean },
@@ -49,63 +49,77 @@ module.exports = async function(callback) {
 
   console.log(`Using registry at ${registry.address}`);
   let cstAddress = await registry.contractForHash(web3.sha3(CST_NAME));
+
+  let cst, cstRegistry, cstFrozen, cstDeprecated, successor, cstStorageName, cstLedgerName, cstName,
+    cstSymbol, buyPriceWei, sellPriceWei, sellCap, foundation, balanceWei, totalSupply, cstFrozenCount,
+    cstAdminCount, cstSuperAdminCount, cstBuyerCount, cstCustomBuyerCount, cstBuyerPool, cstBalanceLimit;
+
   if (cstAddress === NULL_ADDRESS) {
     console.log(`There is no CST contract resgistered with the Registry at ${registry.address}`);
-    callback();
-    return;
+  } else {
+    cst = await CardStackToken.at(cstAddress);
+
+    cstRegistry = await cst.registry();
+    cstFrozen = await cst.frozenToken();
+    cstDeprecated = await cst.isDeprecated();
+    successor = await cst.successor();
+
+    cstStorageName = await cst.storageName();
+    cstLedgerName = await cst.ledgerName();
+    cstName = await cst.name();
+    cstSymbol = await cst.symbol();
+    buyPriceWei = await cst.buyPrice();
+    sellPriceWei = await cst.sellPrice();
+    sellCap = await cst.sellCap();
+    foundation = await cst.foundation();
+    balanceWei = await web3.eth.getBalance(cst.address);
+    totalSupply = await cst.totalSupply();
+    cstFrozenCount = await cst.totalFrozenAccountsMapping();
+    cstAdminCount = await cst.totalAdminsMapping();
+    cstSuperAdminCount = await cst.totalSuperAdminsMapping();
+    cstBuyerCount = await cst.totalBuyersMapping();
+    cstCustomBuyerCount = await cst.totalCustomBuyersMapping();
+    cstBuyerPool = await cst.cstBuyerPool();
+    cstBalanceLimit = await cst.cstBalanceLimit();
   }
 
   let registryAdminCount = await registry.totalAdminsMapping();
   let registrySuperAdminCount = await registry.totalSuperAdminsMapping();
 
-  let cst = await CardStackToken.at(cstAddress);
+  let storageAddress = await registry.storageForHash(web3.sha3(CST_STORAGE_NAME));
+  let ledgerAddress = await registry.storageForHash(web3.sha3(CST_LEDGER_NAME));
 
-  let cstRegistry = await cst.registry();
-  let cstFrozen = await cst.frozenToken();
-  let cstDeprecated = await cst.isDeprecated();
-  let successor = await cst.successor();
+  let storage, storageAdminCount, storageSuperAdminCount, ledger, totalTokens, totalInCirculation,
+    numAccounts, ledgerAdminCount, ledgerSuperAdminCount;
 
-  let cstStorageName = await cst.storageName();
-  let cstLedgerName = await cst.ledgerName();
-  let cstName = await cst.name();
-  let cstSymbol = await cst.symbol();
-  let buyPriceWei = await cst.buyPrice();
-  let sellPriceWei = await cst.sellPrice();
-  let sellCap = await cst.sellCap();
-  let foundation = await cst.foundation();
-  let balanceWei = await web3.eth.getBalance(cst.address);
-  let totalSupply = await cst.totalSupply();
-  let cstFrozenCount = await cst.totalFrozenAccountsMapping();
-  let cstAdminCount = await cst.totalAdminsMapping();
-  let cstSuperAdminCount = await cst.totalSuperAdminsMapping();
-  let cstBuyerCount = await cst.totalBuyersMapping();
-  let cstCustomBuyerCount = await cst.totalCustomBuyersMapping();
-  let cstBuyerPool = await cst.cstBuyerPool();
-  let cstBalanceLimit = await cst.cstBalanceLimit();
+  if (storageAddress === NULL_ADDRESS) {
+    console.log(`There is no storage contract registered with the Registry at ${registry.address}`);
+  } else {
+    storage = await ExternalStorage.at(storageAddress);
+    storageAdminCount = await storage.totalAdminsMapping();
+    storageSuperAdminCount = await storage.totalSuperAdminsMapping();
+  }
 
-  let storageAddress = await registry.storageForHash(web3.sha3(cstStorageName.toString()));
-  let ledgerAddress = await registry.storageForHash(web3.sha3(cstLedgerName.toString()));
+  if (ledgerAddress === NULL_ADDRESS) {
+    console.log(`There is no ledger contract registered with the Registry at ${registryAddress}`);
+  } else {
+    ledger = await CstLedger.at(ledgerAddress);
+    totalTokens = await ledger.totalTokens();
+    totalInCirculation = await ledger.totalInCirculation();
+    numAccounts = await ledger.ledgerCount();
+    ledgerAdminCount = await ledger.totalAdminsMapping();
+    ledgerSuperAdminCount = await ledger.totalSuperAdminsMapping();
+  }
 
-  let storage = await ExternalStorage.at(storageAddress);
-  let ledger = await CstLedger.at(ledgerAddress);
-
-  let totalTokens = await ledger.totalTokens();
-  let totalInCirculation = await ledger.totalInCirculation();
-  let numAccounts = await ledger.ledgerCount();
-  let ledgerAdminCount = await ledger.totalAdminsMapping();
-  let ledgerSuperAdminCount = await ledger.totalSuperAdminsMapping();
-
-  let storageAdminCount = await storage.totalAdminsMapping();
-  let storageSuperAdminCount = await storage.totalSuperAdminsMapping();
 
   function prettyAddress(address) {
     if (address === registry.address) {
       return `${address} (registry)`;
-    } else if (address === cst.address) {
+    } else if (cst && address === cst.address) {
       return `${address} (cst)`;
-    } else if (address === ledger.address) {
+    } else if (ledger && address === ledger.address) {
       return `${address} (ledger)`;
-    } else if (address === storage.address) {
+    } else if (storage && address === storage.address) {
       return `${address} (storage)`;
     }
 
@@ -114,15 +128,26 @@ module.exports = async function(callback) {
 
   console.log(`
 Contracts:
-  Registry: ${prettyAddress(registry.address)}
-  Storage: ${prettyAddress(storage.address)}
-  Ledger: ${prettyAddress(ledger.address)}
-  CST contract: ${prettyAddress(cst.address)}
+  Registry: ${prettyAddress(registry.address)}`);
+
+  if (storage) {
+    console.log(`  Storage: ${prettyAddress(storage.address)}`);
+  }
+
+  if (ledger) {
+    console.log(`  Ledger: ${prettyAddress(ledger.address)}`);
+  }
+
+  if (cst) {
+    console.log(`  CST contract: ${prettyAddress(cst.address)}`);
+  }
+
+console.log(`
 
 Registry (${registry.address}):
   ${CST_NAME}: ${prettyAddress(cstAddress)}
-  ${cstStorageName.toString()}: ${prettyAddress(storageAddress)}
-  ${cstLedgerName.toString()}: ${prettyAddress(ledgerAddress)}
+  ${CST_STORAGE_NAME}: ${prettyAddress(storageAddress)}
+  ${CST_LEDGER_NAME}: ${prettyAddress(ledgerAddress)}
 
   Registry super admins:`);
   for (let i = 0; i < registrySuperAdminCount; i++) {
@@ -142,7 +167,8 @@ Registry (${registry.address}):
     }
   }
 
-  console.log(`
+  if (ledger) {
+    console.log(`
 
 Ledger (${ledger.address})
   totalTokens: ${totalTokens}
@@ -150,44 +176,49 @@ Ledger (${ledger.address})
   number of accounts: ${numAccounts}
 
   Ledger super admins:`);
-  for (let i = 0; i < ledgerSuperAdminCount; i++) {
-    let address = await ledger.superAdminsForIndex(i);
-    let isAdmin = await ledger.superAdmins(address);
-    if (isAdmin) {
-      console.log(`    ${prettyAddress(address)}`);
+    for (let i = 0; i < ledgerSuperAdminCount; i++) {
+      let address = await ledger.superAdminsForIndex(i);
+      let isAdmin = await ledger.superAdmins(address);
+      if (isAdmin) {
+        console.log(`    ${prettyAddress(address)}`);
+      }
     }
-  }
-  console.log(`
+    console.log(`
   Ledger admins:`);
-  for (let i = 0; i < ledgerAdminCount; i++) {
-    let address = await ledger.adminsForIndex(i);
-    let isAdmin = await ledger.admins(address);
-    if (isAdmin) {
-      console.log(`    ${prettyAddress(address)}`);
+    for (let i = 0; i < ledgerAdminCount; i++) {
+      let address = await ledger.adminsForIndex(i);
+      let isAdmin = await ledger.admins(address);
+      if (isAdmin) {
+        console.log(`    ${prettyAddress(address)}`);
+      }
     }
   }
 
-  console.log(`
+  if (storage) {
+    console.log(`
 
 Storage (${storage.address})
   Storage super admins:`);
-  for (let i = 0; i < storageSuperAdminCount; i++) {
-    let address = await storage.superAdminsForIndex(i);
-    let isAdmin = await storage.superAdmins(address);
-    if (isAdmin) {
-      console.log(`    ${prettyAddress(address)}`);
+    for (let i = 0; i < storageSuperAdminCount; i++) {
+      let address = await storage.superAdminsForIndex(i);
+      let isAdmin = await storage.superAdmins(address);
+      if (isAdmin) {
+        console.log(`    ${prettyAddress(address)}`);
+      }
     }
-  }
-  console.log(`
+    console.log(`
   Storage admins:`);
-  for (let i = 0; i < storageAdminCount; i++) {
-    let address = await storage.adminsForIndex(i);
-    let isAdmin = await storage.admins(address);
-    if (isAdmin) {
-      console.log(`    ${prettyAddress(address)}`);
+    for (let i = 0; i < storageAdminCount; i++) {
+      let address = await storage.adminsForIndex(i);
+      let isAdmin = await storage.admins(address);
+      if (isAdmin) {
+        console.log(`    ${prettyAddress(address)}`);
+      }
     }
   }
-  console.log(`
+
+  if (cst) {
+    console.log(`
 
 Cardstack Token (${cst.address}):
   registry: ${prettyAddress(cstRegistry)}
@@ -208,49 +239,50 @@ Cardstack Token (${cst.address}):
   foundation: ${foundation}
 
   CST super admins:`);
-  for (let i = 0; i < cstSuperAdminCount; i++) {
-    let address = await cst.superAdminsForIndex(i);
-    let isAdmin = await cst.superAdmins(address);
-    if (isAdmin) {
-      console.log(`    ${prettyAddress(address)}`);
+    for (let i = 0; i < cstSuperAdminCount; i++) {
+      let address = await cst.superAdminsForIndex(i);
+      let isAdmin = await cst.superAdmins(address);
+      if (isAdmin) {
+        console.log(`    ${prettyAddress(address)}`);
+      }
     }
-  }
-  console.log(`
+    console.log(`
   CST admins:`);
-  for (let i = 0; i < cstAdminCount; i++) {
-    let address = await cst.adminsForIndex(i);
-    let isAdmin = await cst.admins(address);
-    if (isAdmin) {
-      console.log(`    ${prettyAddress(address)}`);
+    for (let i = 0; i < cstAdminCount; i++) {
+      let address = await cst.adminsForIndex(i);
+      let isAdmin = await cst.admins(address);
+      if (isAdmin) {
+        console.log(`    ${prettyAddress(address)}`);
+      }
     }
-  }
-  console.log(`
+    console.log(`
   CST Buyers with custom balance limit:`);
-  for (let i = 0; i < cstCustomBuyerCount; i++) {
-    let address = await cst.customBuyerForIndex(i);
-    let limit = await cst.customBuyerLimit(address);
-    limit = limit.toNumber();
-    if (limit) {
-      console.log(`    ${prettyAddress(address)}: ${(limit/cstBuyerPool.toNumber()) * 100}% (${limit} CST)`);
+    for (let i = 0; i < cstCustomBuyerCount; i++) {
+      let address = await cst.customBuyerForIndex(i);
+      let limit = await cst.customBuyerLimit(address);
+      limit = limit.toNumber();
+      if (limit) {
+        console.log(`    ${prettyAddress(address)}: ${(limit/cstBuyerPool.toNumber()) * 100}% (${limit} CST)`);
+      }
     }
-  }
-  console.log(`
+    console.log(`
   CST Buyers:`);
-  for (let i = 0; i < cstBuyerCount; i++) {
-    let address = await cst.approvedBuyerForIndex(i);
-    let isBuyer = await cst.approvedBuyer(address);
-    if (isBuyer) {
-      console.log(`    ${prettyAddress(address)}`);
+    for (let i = 0; i < cstBuyerCount; i++) {
+      let address = await cst.approvedBuyerForIndex(i);
+      let isBuyer = await cst.approvedBuyer(address);
+      if (isBuyer) {
+        console.log(`    ${prettyAddress(address)}`);
+      }
     }
-  }
 
-  console.log(`
+    console.log(`
   Frozen Accounts:`);
-  for (let i = 0; i < cstFrozenCount; i++) {
-    let address = await cst.frozenAccountForIndex(i);
-    let isFrozen = await cst.frozenAccount(address);
-    if (isFrozen) {
-      console.log(`    ${address}`);
+    for (let i = 0; i < cstFrozenCount; i++) {
+      let address = await cst.frozenAccountForIndex(i);
+      let isFrozen = await cst.frozenAccount(address);
+      if (isFrozen) {
+        console.log(`    ${address}`);
+      }
     }
   }
 
