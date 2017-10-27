9433d884c5e8bf25ab5e5b4758d5038b8a5a5a10
Hassan Abdel-Rahman <hassan.abdelrahman@gmail.com>
2017-10-27 15:34:07 -0400

    Update truffle.js

--- truffle.js
+++ truffle.js
@@ -17,7 +17,7 @@ module.exports = {
     rinkeby: {
       host: "localhost", // Connect to geth on the specified
       port: 8545,
-      from: "0x1E65F71b024937b988fdba09814d60049e0Fc59d",
+      from: "0xf5467A3565995F276F51A74D396F7f9079FF43cB",
       network_id: 4,
       gasPrice: 30000000000 // Default is 100 GWEI (100000000000), which is very fast
     },
