a72bfc14ffaaaa734ecc6699cfe9a7cf7ebfbc37
Hassan Abdel-Rahman <hassan.abdelrahman@gmail.com>
2018-05-31 19:33:09 -0400

    added CSV format for ledger output

--- scripts/ledger-info.js
+++ scripts/ledger-info.js
@@ -1,6 +1,7 @@
 const { CST_NAME } = require("../lib/constants");
 const commandLineArgs = require('command-line-args');
 const getUsage = require('command-line-usage');
+const fs = require('fs');
 let CardStackToken = artifacts.require("./CardStackToken.sol");
 let RegistryContract = artifacts.require("./Registry.sol");
 let CstLedger = artifacts.require("./CstLedger.sol");
@@ -9,6 +10,7 @@ const optionsDefs = [
   { name: "help", alias: "h", type: Boolean },
   { name: "network", type: String },
   { name: "address", alias: "a", type: String },
+  { name: "csv", type: String },
   { name: "registry", alias: "r", type: String }
 ];
 
@@ -25,6 +27,9 @@ const usage = [
     },{
       name: "network",
       description: "The blockchain that you wish to use. Valid options are `testrpc`, `rinkeby`, `mainnet`."
+    },{
+      name: "csv",
+      description: "The CSV file to write to"
     },{
       name: "address",
       alias: "a",
@@ -48,6 +53,7 @@ module.exports = async function(callback) {
 
   let registryAddress = options.registry;
   let queryAddress = options.address;
+  let { csv:csvFile } = options;
 
   let registry = registryAddress ? await RegistryContract.at(registryAddress) : await RegistryContract.deployed();
 
@@ -65,7 +71,21 @@ module.exports = async function(callback) {
   let buyPriceWei = await cst.buyPrice();
   let sigDigits = 6;
 
-    console.log(`
+  if (csvFile) {
+    console.log(`Writing file ${csvFile}...`);
+    fs.writeFileSync(csvFile, `"address","ETH","CARD"\n`, 'ascii');
+    for (let i = 0; i < numAccounts.toNumber(); i++) {
+      let address = await ledger.accountForIndex(i);
+      let balance = await ledger.balanceOf(address);
+      let balanceEth = Math.round(web3.fromWei(buyPriceWei, "ether") * balance * 10 ** sigDigits) / 10 ** sigDigits;
+      fs.appendFileSync(csvFile, `"${address}","${balanceEth}","${balance}"\n`);
+    }
+    console.log("Done");
+    callback();
+    return;
+  }
+
+  console.log(`
 Cardstack Token (${cst.address})
 Ledger (${ledger.address}):
   totalTokens: ${totalTokens} ${cstSymbol}
